7bf819eae56fa6241b2ee6fadcfcb94e041cf457
LeeKangHyun-lab <ka09068@gmail.com>
2025-05-13 14:19:22 +0900

    pull용 커밋

--- src/main/java/org/example/simplememo/service/CommentService.java
+++ src/main/java/org/example/simplememo/service/CommentService.java
@@ -11,16 +11,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-
 public class CommentService {
     private final CommentRepository commentRepository;
     private final MemoRepository memoRepository;
 
-    public Comment createComment(Long memoId, CommentRequestDto request){
+    public Comment createComment(Long memoId, CommentRequestDto request) {
         Memo memo = memoRepository.findById(memoId)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 메모 아이디" + memoId));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메모 아이디: " + memoId));
         Comment comment = new Comment(request.getContent(), memo);
         return commentRepository.save(comment);
     }
 
+    public Comment updateComment(Long commentId, CommentRequestDto request) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글 아이디: " + commentId));
+        comment.setContent(request.getContent());
+        return commentRepository.save(comment);
+    }
+
+    public  void deleteComment(Long commentId) {
+        commentRepository.deleteById(commentId);
+    }
 }
